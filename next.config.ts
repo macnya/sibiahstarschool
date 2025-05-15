9b0d78d280f3744f338088d4e090f4604cb32ecc
Macbrian Maera <macb1738@gmail.com>
2025-05-15 09:53:19 +0000

    i want to create a rollout but it keeps bringing an error

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 
 import type { NextConfig } from 'next';
 
-const WORKSTATIONS_DOMAIN = "3000-firebase-studio-1747199218566.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev";
+const WORKSTATIONS_DOMAIN_1 = "3000-firebase-studio-1747199218566.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev";
+const WORKSTATIONS_DOMAIN_2 = "firebase-studio-1747199218566.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev";
+
 
 const config: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false, // Changed from true
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false, // Changed from true
   },
   images: {
     remotePatterns: [
@@ -21,8 +23,10 @@ const config: NextConfig = {
     ],
   },
   allowedDevOrigins: [
-    WORKSTATIONS_DOMAIN, // Try without scheme
-    `https://${WORKSTATIONS_DOMAIN}`, // Keep with HTTPS as well
+    `https://${WORKSTATIONS_DOMAIN_1}`,
+     WORKSTATIONS_DOMAIN_1, // Without scheme
+    `https://${WORKSTATIONS_DOMAIN_2}`,
+     WORKSTATIONS_DOMAIN_2, // Without scheme
   ],
 };
 
